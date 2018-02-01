1d8d1654283417d0dd85b4317fee2c0372102c88
Mac <1270201010@qq.com>
2018-02-01 13:27:47 +0800

    修改发布消息页面

--- src/pages/sysMessageList/sysMessageListController.js
+++ src/pages/sysMessageList/sysMessageListController.js
@@ -1,19 +1,35 @@
 import services from './sysMessageListServices'
 import commonServices from '@/server/commonServices'
 import dateFormat from '@/utils/dateFormat'
+import scroll from 'better-scroll'
 
 export default {
 	data() {
 		return {
+			height: 0,
 			suggestDialog: {
 				show: false,
 				model: '', //建议的model
+				title: ''
 			},
 			sysMsgList: []
 		}
 	},
-	created() {
+	/*created() {
 		this._seachSysMsgList()
+	},*/
+	mounted(){
+		this._seachSysMsgList()
+		this.height = window.innerHeight + 'px'
+		this.$nextTick(()=>{
+			if(!this.Scroll){
+				this.Scroll = new scroll(this.$refs['sysMsg'],{
+					click: true
+				})
+			}else{
+				this.Scroll.refresh();
+			}
+		})
 	},
 	methods:{
 		goBack() {
@@ -23,8 +39,33 @@ export default {
 
 		/* 建议 */
 		suggestSubmit() {
+			if(!this.suggestDialog.model||!this.suggestDialog.title){
+				this.suggestDialog.show = true
+				return
+			}
 			//提交建议信息
-			this.suggestDialog.show = false
+			this.suggestDialog.show = false;
+			console.log(this.suggestDialog)
+			services.sendMsg({
+				Vue: this,
+				model: {
+					msg: this.suggestDialog.model,
+					date: (new Date).getTime(),
+					title: this.suggestDialog.title
+				}
+			}).then(res=>{
+				console.log(res)
+				if(res.code == 0){
+					this.$toast('发布成功')
+					this.suggestDialog.model = ''
+					this.suggestDialog.title = ''
+					this._seachSysMsgList()
+					this.Scroll.scrollTo(0, 0)
+					this.suggestDialog.show = false
+				}
+			},err=>{
+				console.log(err)
+			})
 		},
 		suggestCancel() {
 			this.suggestDialog.show = false
@@ -35,11 +76,14 @@ export default {
 				Vue: this,
 			})
 			.then(res=>{
-				if(res.length > 0){
+				/*if(res.length > 0){
 					console.log(res)
 					for(let elem of Object.values(res)){
 						elem.date = dateFormat(new Date(parseInt(elem.date)), 'yyyy-MM-dd hh:mm:ss')
 					}
+					this.$set(this, 'sysMsgList', res.reverse())
+				}*/
+				if(res.length>0){
 					this.$set(this, 'sysMsgList', res.reverse())
 				}
 				
--- src/pages/sysMessageList/sysMessageListServices.js
+++ src/pages/sysMessageList/sysMessageListServices.js
@@ -1,5 +1,19 @@
 import commonServices from '@/server/commonServices'
 
 export default {
-
+    //发送建议
+    sendMsg:({model, Vue, hidenLoading})=>{
+        return new Promise((resolve,reject)=>{
+            commonServices.transport({
+                url: 'admin/addBroadCast',
+				model,
+				Vue,
+				hidenLoading 
+            }).then(res=>{
+                resolve(res)
+            },err=>{
+                reject(err)
+            })
+        })
+    }
 }
